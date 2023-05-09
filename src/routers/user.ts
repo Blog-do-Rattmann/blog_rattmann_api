ee9a0ec95aedf643fcdfb2bcb0c2e036d5fe35ea
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2023-05-09 00:21:37 -0300

    Criação da rota de perfil

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -4,5 +4,6 @@ import user from '../controllers/user';
 const userRoutes = Router();
 
 userRoutes.post('/cadastrar', user.register);
+userRoutes.get('/perfil/:id', user.profile);
 
 export default userRoutes;
