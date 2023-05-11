df9de94c096df685f879d201390a5d4bc78dd1f8
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2023-05-11 00:17:24 -0300

    Criado validação de dados dos campos

---
+++ src/utils/validate.ts
@@ -0,0 +1,30 @@
+const validateData = (value: any) => {
+    if ((value === null || value === undefined) || (typeof value === 'string' && value.trim() === '')) return false;
+
+    return true;
+}
+
+const validateName = (name: string) => {
+    if (name.length < 2 || name.length > 100) return false;
+
+    return true;
+}
+
+const validateUsername = (username: string) => {
+    if (username.length < 4 || username.length > 25) return false;
+
+    return true;
+}
+
+const validateEmail = (email: string) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) return true;
+
+    return false;
+}
+
+export {
+    validateData,
+    validateName,
+    validateUsername,
+    validateEmail
+}
