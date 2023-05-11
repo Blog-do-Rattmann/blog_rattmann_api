914b6aa42bd461aeb3c4ede1b632549bb9dee430
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2023-05-11 00:15:50 -0300

    Criado rota para editar usuário

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import user from '../controllers/user';
 
 const userRoutes = Router();
@@ -6,5 +6,6 @@ const userRoutes = Router();
 userRoutes.post('/cadastrar', user.register);
 userRoutes.get('/perfil/:id', user.profile);
 userRoutes.get('/lista', user.list);
+userRoutes.patch('/editar/:id', user.update);
 
 export default userRoutes;
